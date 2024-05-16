f773e2e0210b2c7f55b43910c9f61d363f7721d1
bhairivi <bhairivi@ideaclan.com>
2024-05-16 15:53:26 +0530

    update: check available data

--- src/components/MedicineDetails/index.tsx
+++ src/components/MedicineDetails/index.tsx
@@ -4,7 +4,7 @@ import { Medicine } from "../Medicines";
 import "./styles.css";
 
 interface FarmType {
-  [key: string]: boolean
+  [key: string]: boolean;
 }
 
 interface Props {
@@ -12,7 +12,7 @@ interface Props {
 }
 const MAX_ITEMS_PER_ROW = 2;
 export const MedicineDetails: FC<Props> = ({ data }) => {
-  const { available_forms = [], salt_forms_json = {}, salt = "" } = data;
+  const { salt_forms_json = {}, salt = "" } = data;
   // state to check active Medicines
   const firstFarm = Object.keys(salt_forms_json)[0];
   const firstStrength = Object.keys(salt_forms_json[firstFarm])[0];
@@ -22,9 +22,13 @@ export const MedicineDetails: FC<Props> = ({ data }) => {
     Strength: firstStrength,
     Packaging: firstPackaging,
   });
-  const [showMore, setShowMore] = useState<FarmType>({Farm:false,Strength:false,Packaging:false});
-   // This function handles active meds data when we select Farm
-   const handleActiveMedsFarms = (salt: string) => {
+  const [showMore, setShowMore] = useState<FarmType>({
+    Farm: false,
+    Strength: false,
+    Packaging: false,
+  });
+  // This function handles active meds data when we select Farm
+  const handleActiveMedsFarms = (salt: string) => {
     setActiveMeds((prev) => {
       const newStrength = Object.keys(salt_forms_json[salt])[0];
       const newPackaging = Object.keys(salt_forms_json[salt][newStrength])[0];
@@ -56,52 +60,70 @@ export const MedicineDetails: FC<Props> = ({ data }) => {
       };
     });
   };
-
-  const handleShowMore = (key:string) => {
-    setShowMore((prev)=>({...prev,[key]:true}));
+  // Above two function handles show and hide functionality
+  const handleShowMore = (key: string) => {
+    setShowMore((prev) => ({ ...prev, [key]: true }));
   };
 
-  const handleShowLess = (key:string) => {
-    setShowMore((prev)=>({...prev,[key]:false}));
+  const handleShowLess = (key: string) => {
+    setShowMore((prev) => ({ ...prev, [key]: false }));
   };
   //This functon handles more and hide section
-  const renderFooter=(saltKeys:string[],key:string): JSX.Element=>{
-   return(<>
-    { saltKeys.length > 2&&
-      (!showMore[key]  ?(
-         <p
-           key="show-more"
-           onClick={() => handleShowMore(key)}
-           style={{ cursor: 'pointer', color: 'blue' }}
-         >
-           ...more
-         </p>
-       ):  <p
-       key="show-more"
-       onClick={() => handleShowLess(key)}
-       style={{ cursor: 'pointer', color: 'blue' }}
-     >
-       ...hide
-     </p>)}
-     </>
-     )
-
-  }
+  const renderFooter = (saltKeys: string[], key: string): JSX.Element => {
+    return (
+      <>
+        {saltKeys.length > 2 &&
+          (!showMore[key] ? (
+            <p
+              key="show-more"
+              onClick={() => handleShowMore(key)}
+              style={{ cursor: "pointer", color: "blue" }}
+            >
+              ...more
+            </p>
+          ) : (
+            <p
+              key="show-more"
+              onClick={() => handleShowLess(key)}
+              style={{ cursor: "pointer", color: "blue" }}
+            >
+              ...hide
+            </p>
+          ))}
+      </>
+    );
+  };
   // This function handles strength data
   const handleStrengthData = (salt: Object): JSX.Element => {
     const saltKeys = Object.keys(salt);
     const displayKeys = showMore?.Strength ? saltKeys : saltKeys.slice(0, 2);
-    return(
+
+    return (
       <>
-        {displayKeys.map((strength) => (
-          <div key={strength} onClick={() => handleActiveMedsStrength(strength)}>
-            <Tag color={activeMeds?.Strength === strength ? 'green' : ''}>
-              {strength}
-            </Tag>
-          </div>
-        ))}
-        {renderFooter(saltKeys,"Strength")}
-      
+        {displayKeys.map((strength) => {
+          let isAvailable = false;
+          // check if there is any available stores near by
+          Object.keys(salt_forms_json[activeMeds?.Farm][strength])?.forEach((packaging) => {
+            Object.keys(salt_forms_json[activeMeds?.Farm][strength][packaging] || {}).forEach(
+              (key: any) => {
+                const medsArray = salt_forms_json[activeMeds?.Farm][strength][packaging][key];
+                if (medsArray !== null) {
+                  isAvailable = true;
+                }
+              }
+            );
+          });
+          return (
+            <div
+              key={strength}
+              onClick={() => handleActiveMedsStrength(strength)}
+              style={{ border: isAvailable ? "" : "red" }}
+            >
+              <Tag color={activeMeds?.Strength === strength ? "green" : ""}>{strength}</Tag>
+            </div>
+          );
+        })}
+        {renderFooter(saltKeys, "Strength")}
       </>
     );
   };
@@ -111,18 +133,35 @@ export const MedicineDetails: FC<Props> = ({ data }) => {
     const strengthKeys = Object.keys(strength);
     const displayKeys = showMore?.Packaging ? strengthKeys : strengthKeys.slice(0, 2);
     const elements = displayKeys.map((packaging) => {
+      let isAvailable = false;
+      // check if there is any available stores near by
+      Object.keys(salt_forms_json[activeMeds?.Farm][activeMeds?.Strength][packaging] || {}).forEach(
+        (key: any) => {
+          const medsArray = salt_forms_json[activeMeds?.Farm][activeMeds?.Strength][packaging][key];
+          if (medsArray !== null) {
+            isAvailable = true;
+          }
+        }
+      );
       return (
-        <div key={packaging} onClick={() => handleActiveMedsPackaging(packaging)}>
+        <div
+          key={packaging}
+          onClick={() => handleActiveMedsPackaging(packaging)}
+          style={{ border: isAvailable ? "1px solid black" : "3px solid red" }}
+        >
           <Tag color={activeMeds?.Packaging === packaging ? "green" : ""}>{packaging}</Tag>
         </div>
       );
     });
-    return <>{elements}
-          {renderFooter(strengthKeys,"Packaging")}
-      </>;
+    return (
+      <>
+        {elements}
+        {renderFooter(strengthKeys, "Packaging")}
+      </>
+    );
   };
-   // This function handles logic to find minimum price of meds and if its out of stock
-   const handlePharmacyPrice = (packaging: {
+  // This function handles logic to find minimum price of meds and if its out of stock
+  const handlePharmacyPrice = (packaging: {
     [key: string]: { selling_price: number | null }[] | null;
   }): JSX.Element => {
     let min = Infinity;
@@ -141,23 +180,41 @@ export const MedicineDetails: FC<Props> = ({ data }) => {
 
     return <>{displayMin}</>;
   };
+  // Render Farm
   const renderSaltFarms = () => {
     const farmKeys = Object.keys(saltForms);
     const displayKeys = showMore?.Farm ? farmKeys : farmKeys.slice(0, 2);
     return (
       <>
-        {displayKeys.map((farm) => (
-          <div key={farm} onClick={() => handleActiveMedsFarms(farm)}>
-            <Tag color={activeMeds?.Farm === farm ? 'green' : ''}>
-              {farm}
-            </Tag>
-          </div>
-        ))}
-        {renderFooter(farmKeys,"Farm")}
+        {displayKeys.map((farm) => {
+          const isSelected: boolean = activeMeds?.Farm === farm;
+          let isAvailable = false;
+          // check if there is any available stores near by
+          Object.keys(salt_forms_json[farm])?.forEach((strength) => {
+            Object.keys(salt_forms_json[farm][strength])?.forEach((packaging) => {
+              Object.keys(salt_forms_json[farm][strength][packaging] || {}).forEach((key: any) => {
+                const medsArray = salt_forms_json[farm][strength][packaging][key];
+                if (medsArray !== null) {
+                  isAvailable = true;
+                }
+              });
+            });
+          });
+          return (
+            <div
+              key={farm}
+              onClick={() => handleActiveMedsFarms(farm)}
+              style={{ borderColor: isAvailable ? "" : "red" }}
+            >
+              <Tag color={isSelected ? "green" : ""}>{farm}</Tag>
+            </div>
+          );
+        })}
+        {renderFooter(farmKeys, "Farm")}
       </>
     );
   };
-  
+  //Render Strength
   const renderSaltStrength = () => {
     return (
       <Row className="second-row" style={{ flex: 1 }}>
@@ -166,18 +223,17 @@ export const MedicineDetails: FC<Props> = ({ data }) => {
       </Row>
     );
   };
-
+  // Render packaging
   const renderSaltPackaging = () => {
-    return <Row className="third-row" style={{ flex: 1 }}>
-      <div className="medicine-div-label">Packaging:</div>
-      <div className="medicine-div-value">
-        {handlePackagingData(saltForms[activeMeds?.Farm][activeMeds?.Strength])}
-      </div>
-    </Row>;
+    return (
+      <Row className="third-row" style={{ flex: 1 }}>
+        <div className="medicine-div-label">Packaging:</div>
+        <div className="medicine-div-value">
+          {handlePackagingData(saltForms[activeMeds?.Farm][activeMeds?.Strength])}
+        </div>
+      </Row>
+    );
   };
-  const handleAvailableMedicines = () => {};
- 
-  
 
   const saltForms: any = salt_forms_json;
   return (
@@ -185,8 +241,8 @@ export const MedicineDetails: FC<Props> = ({ data }) => {
       {activeMeds?.Farm ? (
         <Card>
           <div className="medicine-card">
-            {/* render farm data */}
             <Col style={{ flex: 1 }}>
+              {/* render farm data */}
               <Row className="first-row">
                 <div className="medicine-div-label">Form:</div>
                 <div className="medicine-div-value">{renderSaltFarms()}</div>
